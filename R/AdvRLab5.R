e208cb22df3f7e5912ee2ca43f7deb82c884da58
Philhoels <phil_hoelscher@web.de>
2018-10-07 13:16:24 +0200

    update package description

--- R/AdvRLab5.R
+++ R/AdvRLab5.R
@@ -1,4 +1,4 @@
-#' AdvRLab5: A package we have to create in lab 3 of advanced R Programming
+#' AdvRLab5: A package we have to create in lab 5 of advanced R Programming
 #'
 #' @section TwitterWordCould function:
 #' This function creates a data frame which can be used to create a word cloud
